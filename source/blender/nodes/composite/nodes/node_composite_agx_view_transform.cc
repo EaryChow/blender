b99d0d4199fe3fbca89676233d248c8e4fe7ec2c
EaryChow <59176246+EaryChow@users.noreply.github.com>
2025-05-31 02:43:16 +0800

    remove enum name legacy registration

--- source/blender/nodes/composite/nodes/node_composite_agx_view_transform.cc
+++ source/blender/nodes/composite/nodes/node_composite_agx_view_transform.cc
@@ -51,7 +51,6 @@ static void node_register()
   cmp_node_type_base(&ntype, "CompositorNodeAgXViewTransform");
   ntype.ui_name = "AgX View Transform";
   ntype.ui_description = "Applies AgX Picture Formation that converts rendered RGB exposure into an Image for Display";
-  ntype.enum_name_legacy = "AGX_VIEW_TRANSFORM";
   ntype.nclass = NODE_CLASS_OP_COLOR;
   ntype.declare = node_declare;
   blender::bke::node_type_size_preset(ntype, blender::bke::eNodeSizePreset::Large);

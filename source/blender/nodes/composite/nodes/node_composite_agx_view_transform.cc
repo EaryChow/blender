34ab7fe5ee2961811689a89dd908eaebe9c87f2a
EaryChow <59176246+EaryChow@users.noreply.github.com>
2025-06-21 23:36:33 +0800

    try passing all input sockets to GLSL even if unused

--- source/blender/nodes/composite/nodes/node_composite_agx_view_transform.cc
+++ source/blender/nodes/composite/nodes/node_composite_agx_view_transform.cc
@@ -489,33 +489,22 @@ static int node_gpu_material(GPUMaterial *material,
   const float3x3 working_to_rec2020 = RGBtoRGB(COLOR_SPACE_PRI[static_cast<int>(node->custom2)], REC2020_PRI);
   const float3x3 display_to_rec2020 = RGBtoRGB(COLOR_SPACE_PRI[static_cast<int>(node->custom4)], REC2020_PRI);
 
-  GPUNodeStack reconstructed_inputs[19];
-  filtered_inputs[0] = inputs[0]; /* Color */
-  filtered_inputs[1] = inputs[1]; /* Log2 Minimum Exposure */
-  filtered_inputs[2] = inputs[2]; /* Log2 Maximum Exposure */
-  filtered_inputs[3] = inputs[3]; /* General Contrast */
-  filtered_inputs[4] = inputs[4]; /* Toe Contrast */
-  filtered_inputs[5] = inputs[5]; /* Shoulder Contrast */
-  filtered_inputs[6] = inputs[6]; /* Contrast Pivot Offset */
-  filtered_inputs[7] = inputs[11]; /* Per-Channel Hue Flight */
-  filtered_inputs[8] = inputs[14]; /* Compensate for the Negatives */
-  filtered_inputs[9] = (float)node->custom3;
-  filtered_inputs[10] = blender::float4x4(scene_linear_to_working);
-  filtered_inputs[11] = blender::float4x4(working_to_display);
-  filtered_inputs[12] = blender::float4x4(display_to_scene_linear);
-  filtered_inputs[13] = log_midgray;
-  filtered_inputs[14] = midgray;
-  filtered_inputs[15] = blender::float4x4(insetmat);
-  filtered_inputs[16] = blender::float4x4(outsetmat);
-  filtered_inputs[17] = blender::float4x4(working_to_rec2020);
-  filtered_inputs[18] = blender::float4x4(display_to_rec2020);
-
-
+  float p_working_log = (float)node->custom3;
   return GPU_stack_link(material, 
                         node, 
                         "node_composite_agx_view_transform", 
-                        reconstructed_inputs, 
-                        outputs);
+                        inputs, 
+                        outputs, 
+                        GPU_uniform(&p_working_log), /* p_working_log */
+                        GPU_uniform(blender::float4x4(scene_linear_to_working).base_ptr()),
+                        GPU_uniform(blender::float4x4(working_to_display).base_ptr()),
+                        GPU_uniform(blender::float4x4(display_to_scene_linear).base_ptr()),
+                        GPU_uniform(&log_midgray),
+                        GPU_uniform(&midgray),
+                        GPU_uniform(blender::float4x4(insetmat).base_ptr()),
+                        GPU_uniform(blender::float4x4(outsetmat).base_ptr()),
+                        GPU_uniform(blender::float4x4(working_to_rec2020).base_ptr()),
+                        GPU_uniform(blender::float4x4(display_to_rec2020).base_ptr()));
 }
 
 // Multi Function
